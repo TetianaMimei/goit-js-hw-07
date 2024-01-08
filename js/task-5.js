617399cb56b5a7d86915cc90d1729a3df1d78732
TetianaMimei <tanyamimey@gmail.com>
2024-01-08 17:33:47 +0100

    update hw

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector('.color');
+const buttonChange = document.querySelector('.change-color');
+const bodyColor = document.querySelector('.widget');
+buttonChange.addEventListener('click', onButtonClick);
+
+function onButtonClick(e) {
+  const newColor = getRandomHexColor();
+  changeColor.textContent = newColor;
+  bodyColor.style.backgroundColor = newColor;
+};
+
--- js/task-6.js
+++ js/task-6.js
@@ -2,4 +2,36 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const inputCreate = document.querySelector('.controls-input');
+const buttonCreate = document.querySelector('.controls-create');
+const buttonDestroy = document.querySelector('.controls-destroy');
+const containerCreate = document.querySelector('#boxes');
+
+buttonCreate.addEventListener('click', onButtonClick);
+buttonDestroy.addEventListener('click', destroyBoxes);
+
+function onButtonClick(e) {
+  createBoxes(inputCreate.value);
+};
+
+function createBoxes(amount) {
+  if (inputCreate.value >= 1 && inputCreate.value <= 100) {
+    containerCreate.innerHTML = '';
+    for (let i = 0; i < amount; i += 1) {
+    const createDiv = document.createElement('div');
+    createDiv.style.backgroundColor = getRandomHexColor();
+    createDiv.style.width = `${30 + i * 10}px`;
+    createDiv.style.height = `${30 + i * 10}px`;
+    containerCreate.appendChild(createDiv);
+    };
+    inputCreate.value = '';
+  }
+};
+
+function destroyBoxes() {
+  inputCreate.value = '';
+  containerCreate.innerHTML = '';
+};
+
